076bf1b9bad6b3443cc48b9f29e77bfe4ad625ea
d.lursmanashvili <datolursmanishvili64@gmail.com>
2023-08-26 18:39:47 +0400

    add code

--- Template/Program.cs
+++ Template/Program.cs
@@ -5,36 +5,6 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContext");
-//builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
-//DI.DependecyResolver(builder.Services);
-
-//// Add services to the container.
-
-//builder.Services.AddControllers();
-//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-//app.UseHttpsRedirection();
-
-//app.UseAuthorization();
-
-//app.MapControllers();
-
-//app.Run();
-
-
-
 var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContext");
 var services = builder.Services;
 
